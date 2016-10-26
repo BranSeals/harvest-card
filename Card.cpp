f030d7abfc9f1beb37aa0fc906f81070df8a310e
Brandon Seals <brandon.seals@gmail.com>
2016-10-25 21:41:50 -0400

    Add getter and setter functions for Card class

--- Card.cpp
+++ Card.cpp
@@ -18,3 +18,63 @@ Card::~Card()
 {
     std::cout << "\n> Card destructed\n";
 }
+
+//* Getters / Setters *//
+
+/* Name of card */
+std::string Card::getCardName(void)
+{
+    return cardName;
+}
+
+void Card::setCardName(std::string name)
+{
+    cardName = name;
+}
+
+/* Effect on card, referring to list of effects */
+int Card::getCardEffect(void)
+{
+    return cardEffect;
+}
+
+void Card::setCardEffect(int effect)
+{
+    cardEffect = effect;
+}
+
+/* Cost of card in Market */
+int Card::getCardCost(void)
+{
+    return cardCost;
+}
+
+void Card::setCardCost(int cost)
+{
+    cardCost = cost;
+};
+
+/* Value of card when sold */
+int Card::getCardValue(void)
+{
+    return cardValue;
+}
+
+void Card::setCardValue(int value)
+{
+    cardValue = value;
+}
+
+/* Description of card */
+std::string Card::getCardDescription(void)
+{
+    return cardDescription;
+}
+
+void Card::setCardDescription(std::string description)
+{
+    cardDescription = description;
+}
+
+//* Methods *//
+
