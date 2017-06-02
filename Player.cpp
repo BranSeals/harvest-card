972b0a09af9c2d6cc1887cd34c15bf1c82c5684c
Bran Seals <brandon.seals@gmail.com>
2017-06-02 16:37:53 -0400

    Fix indentation formatting
    Between using multiple editors over multiple systems, at some point tabs were used instead of spaces-as-tabs. This did not show up in the editors I was using, but does on GitHub.

--- Player.cpp
+++ Player.cpp
@@ -83,105 +83,105 @@ void Player::printFarm(void)
 
 void Player::buy(Market* market)
 {
-	int selection = select("Buy anything?", 1, 9);
+    int selection = select("Buy anything?", 1, 9);
 
-	if (selection == 0) {
-		advancePhase();
-		return;
-	}
+    if (selection == 0) {
+	advancePhase();
+	return;
+    }
 
-	if (selection && market->canSelect(selection)) {
-		if (getPlayerGold() > market->getCostAt(selection)) {
-			boughtCard = market->removeFromStall(selection);
-			removeGold(boughtCard.getCardCost());
-			playerFarm.addCard(boughtCard);
-			std::cout << "\n> Bought " << boughtCard.getCardName() << " for "
-				<< boughtCard.getCardCost() << " gold.\n";
-		} else {
-			std::cout << "\nNot enough gold.";
-		}
+    if (selection && market->canSelect(selection)) {
+	if (getPlayerGold() > market->getCostAt(selection)) {
+	    boughtCard = market->removeFromStall(selection);
+	    removeGold(boughtCard.getCardCost());
+	    playerFarm.addCard(boughtCard);
+	    std::cout << "\n> Bought " << boughtCard.getCardName() << " for "
+		      << boughtCard.getCardCost() << " gold.\n";
+	} else {
+	    std::cout << "\nNot enough gold.";
 	}
+    }
 }
 
 int Player::advancePhase(void)
 {
-	return ++playerPhase;
+    return ++playerPhase;
 }
 
 int Player::getPlayerPhase(void)
 {
-	return playerPhase;
+    return playerPhase;
 }
 void Player::setPlayerPhase(int phase)
 {
-	playerPhase = phase;
+    playerPhase = phase;
 }
 
 void Player::sellProduct()
 {
     addGold(playerFarm.sellProduct());
-	advancePhase();
+    advancePhase();
 }
 
 int Player::select(std::string message, int low, int high) {
-	if (low > high) {
-		std::cout << "\n*** Error, first number must be lower than second ***\n";
-		return 0;
+    if (low > high) {
+	std::cout << "\n*** Error, first number must be lower than second ***\n";
+	return 0;
+    }
+
+    int selection{0};
+    do {
+	if (low == high) {
+	    std::cout << "\n" << message << " [" << high << ", 0 to cancel] ";
+	} else {
+	    std::cout << "\n" << message << " [" << low << "-" << high << ", 0 to cancel] ";
 	}
-	
-	int selection{0};
-	do {
-		if (low == high) {
-			std::cout << "\n" << message << " [" << high << ", 0 to cancel] ";
-		} else {
-			std::cout << "\n" << message << " [" << low << "-" << high << ", 0 to cancel] ";
-		}
-		std::cin >> selection;
-		std::cin.clear();
-		std::cin.ignore();
-	} while ((selection < low || selection > high) && (selection != 0));
-	return selection;
+	std::cin >> selection;
+	std::cin.clear();
+	std::cin.ignore();
+    } while ((selection < low || selection > high) && (selection != 0));
+    return selection;
 }
 
 void Player::work(void)
 {
-	printFarm();
+    printFarm();
 
-	/* Make sure there are tools to work with */
-	if (playerFarm.sizeOf("Tool") == 0) {
-		std::cout << "\n> No tools to work with.\n";
-		advancePhase();
-		return;
-	}
+    /* Make sure there are tools to work with */
+    if (playerFarm.sizeOf("Tool") == 0) {
+	std::cout << "\n> No tools to work with.\n";
+	advancePhase();
+	return;
+    }
 
-	/* Player selects a tool - automatically reduces number to proper tool index */
-	// TODO : separate selection and selected tool variables; use selection in case it is 0 to cancel
-	int selection = select("Use which tool?", 1 + playerFarm.sizeOf("Seed"), playerFarm.sizeOf("Seed") + playerFarm.sizeOf("Tool"));
-	if (selection == 0) {
-		advancePhase();
-		return;
-	}
+    /* Player selects a tool - automatically reduces number to proper tool index */
+    // TODO : separate selection and selected tool variables; use selection in case it is 0 to cancel
+    int selection = select("Use which tool?", 1 + playerFarm.sizeOf("Seed"), playerFarm.sizeOf("Seed") + playerFarm.sizeOf("Tool"));
+    if (selection == 0) {
+	advancePhase();
+	return;
+    }
 
-	int selectedTool = selection - playerFarm.sizeOf("Seed") - 1;
+    int selectedTool = selection - playerFarm.sizeOf("Seed") - 1;
 
-	/* Checks if a tool can be used */
-	if (!playerFarm.canSelectTool(selectedTool)) {
-		return;
-	}
+    /* Checks if a tool can be used */
+    if (!playerFarm.canSelectTool(selectedTool)) {
+	return;
+    }
 
-	/* Player selects a target */
-	int selectedTarget = select("Use tool on which card?", 1, playerFarm.sizeOf("Seed") + playerFarm.sizeOf("Tool") + playerFarm.sizeOf("Livestock"));
+    /* Player selects a target */
+    int selectedTarget = select("Use tool on which card?", 1, playerFarm.sizeOf("Seed") + playerFarm.sizeOf("Tool") + playerFarm.sizeOf("Livestock"));
 
-	/* Work farm - if target is improper; workFarm will say so */
-	playerFarm.workFarm(selectedTool, selectedTarget);
+    /* Work farm - if target is improper; workFarm will say so */
+    playerFarm.workFarm(selectedTool, selectedTarget);
 }
 
 void Player::refreshTools(void)
 {
-	playerFarm.refreshTools();
+    playerFarm.refreshTools();
 }
 
 void Player::harvestCrops(int season)
 {
-	addGold(playerFarm.harvest(season));
+    addGold(playerFarm.harvest(season));
 }
