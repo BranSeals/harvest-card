5a6d385a8295ca4bac1b2c5fefbd5b7711828a2b
Bran Seals <brandon.seals@gmail.com>
2017-06-19 19:14:24 -0400

    Create CardEdit files
    These files can be used to easily update the list of cards in the game.
    Will have the ability to add, edit, or remove cards from cards.txt
    through the use of an easy-to-read and easy-to-edit plaintext file
    named editCards.txt

---
+++ CardEdit.cpp
@@ -0,0 +1,49 @@
+// Copyright (C) 2016. All rights reserved.
+// Created: 2017-06-19
+
+#include "CardEdit.hpp"
+
+// Load cardEdit.txt into memory
+void CardEdit::loadEditList()
+{
+    //fill editList with contents of cardEdit.txt
+}
+
+// Load current cards.txt file into memory
+void CardEdit::loadOldList()
+{
+    //fill oldList with contents of cards.txt
+}
+
+// Create new list in memory using cardEdit cards with cards.txt format
+void CardEdit::createNewList()
+{
+    
+}
+
+// Returns true if cardEdit.txt is properly formatted
+bool CardEdit::verifyEditList()
+{
+    
+    return false;
+}
+
+// Returns true if cards.txt is properly formatted and sorted
+bool CardEdit::verifyOldList()
+{
+    
+    return false;
+}
+
+// Creates a line of card attributes in cards.txt format
+std::string CardEdit::createCardLine()
+{
+    
+    return "";
+}
+
+// Use newList to update cards.txt with new additions or removals
+void CardEdit::updateCardFile()
+{
+    
+}
---
+++ CardEdit.hpp
@@ -0,0 +1,30 @@
+// Copyright (C) 2016. All rights reserved.
+// Created: 2017-06-19
+
+// Purpose: takes file named editCards.txt used for easier editing of cards
+// and translates into format used in the project. Can be used to quickly
+// add, update, or remove cards.
+
+#ifndef CardEdit_hpp
+#define CardEdit_hpp
+
+#include <string>
+#include <vector>
+
+class CardEdit
+{
+public:
+    void loadEditList();
+    void loadOldList();
+    void createNewList();
+    bool verifyEditList();
+    bool verifyOldList();
+    std::string createCardLine();
+    void updateCardFile();
+private:
+    std::vector<std::string> editList;
+    std::vector<std::string> oldList;
+    std::vector<std::string> newList;
+    std::string cardLine;
+};
+#endif
