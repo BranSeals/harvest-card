5f97e63e7031c44fb91b0c16b6ecfbc6f5e94401
Bran Seals <brandon.seals@gmail.com>
2017-06-19 23:41:37 -0400

    Fix big in repopulateEditFile and allow writing of files

--- CardEdit.cpp
+++ CardEdit.cpp
@@ -14,6 +14,7 @@ CardEdit::~CardEdit()
 // Load cardEdit.txt into memory
 void CardEdit::loadEditList()
 {
+    editList.clear();
     //fill editList with contents of cardEdit.txt
     std::ifstream inFile;
     std::string lineContent{""};
@@ -22,7 +23,9 @@ void CardEdit::loadEditList()
 
     while (!inFile.eof()) {
         getline(inFile, lineContent);
-        editList.push_back(lineContent);
+        if (lineContent != "") {
+            editList.push_back(lineContent);
+        }
     }
 }
 
@@ -46,9 +49,9 @@ void CardEdit::createNewList()
 {
     std::vector<std::string> fields;
     // for each element in editList, do createCardLine and add to newList
-    for (int i{0}; i < editList.size(); i += 10) {
+    for (int i{0}; i < editList.size(); i += 9) {
         for (int j{0}; j < 9; ++j) {
-            fields.push_back(editList[i + j]);
+            fields.push_back( editList[i + j].substr(editList[i + j].find_first_of(":") + 2));
         }
         newList.push_back(createCardLine(fields));
         fields.clear();
@@ -85,6 +88,13 @@ void CardEdit::updateCardFile()
     // if card ID exists, check if info the same
     // if card ID doesn't exist, insert new card sorted
     // do a pass through cards.txt - if any IDs not in newList, remove from cards.txt
+
+    std::ofstream outFile;
+    outFile.open("cards.txt", std::ios::out | std::ios::trunc);
+    for (int i{ 0 }; i < newList.size(); ++i) {
+        outFile << newList[i] + "\n";
+    }
+    outFile.close();
 }
 
 // Repopulate cardEdit.txt using current cards.txt
@@ -103,7 +113,7 @@ void CardEdit::repopulateEditFile()
     }
 
     for (size_t i{0}; i < currentList.size(); ++i) {
-        fields = split(lineContent);
+        fields = split(currentList[i]);
         editList.push_back("Card ID: " + fields[0]);
         editList.push_back("Season: " + fields[1]);
         editList.push_back("Name: " + fields[2]);
@@ -112,13 +122,12 @@ void CardEdit::repopulateEditFile()
         editList.push_back("Target: " + fields[5]);
         editList.push_back("Value: " + fields[6]);
         editList.push_back("Cost: " + fields[7]);
-        editList.push_back("Initial Face Value: " + fields[8]);
-        editList.push_back("\n");
+        editList.push_back("Initial Face Value: " + fields[8] + "\n");
     }
     inFile.close();
 
     std::ofstream outFile;
-    outFile.open("cardEdit.txt", std::ios::out);
+    outFile.open("cardEdit.txt", std::ios::out | std::ios::trunc);
     for (int i{0}; i < editList.size(); ++i) {
         outFile << editList[i] + "\n";
     }
