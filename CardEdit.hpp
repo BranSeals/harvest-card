c00d3e0461fb74169ad1fa61619060753a1b3366
Bran Seals <brandon.seals@gmail.com>
2017-06-20 02:33:49 -0400

    Reorder functions for clarity and rewrite Purpose

--- CardEdit.hpp
+++ CardEdit.hpp
@@ -1,13 +1,13 @@
 // Copyright (C) 2016. All rights reserved.
 // Created: 2017-06-19
 
-// Purpose: takes file named editCards.txt used for easier editing of cards
-// and translates into format used in the project. Can be used to quickly
-// add, update, or remove cards.
+// Purpose: Update cards.txt using cards_edit.txt or vice-versa.
+//          cards_edit.txt is formatted for easy editing.
 
 #ifndef CardEdit_hpp
 #define CardEdit_hpp
 
+#include <iostream>
 #include <string>
 #include <vector>
 #include <fstream>
@@ -17,16 +17,25 @@ class CardEdit
 public:
     CardEdit();
     ~CardEdit();
+
     void loadEditList();
     void loadCardList();
+
     void convertEditToCardList();
+    void convertCardToEditList();
+
     void verifyEditList();
     void verifyCardList();
-    std::string createCardLine(std::vector<std::string>);
-    void writeCardFile();
+
     void writeEditFile();
-    std::vector<std::string> split(std::string);
+    void writeCardFile();
+
+    void updateEditFile();
     void updateCardFile();
+
+    std::string shrink(std::vector<std::string>);
+    std::vector<std::string> split(std::string);
+
 private:
     std::vector<std::string> editList;
     std::vector<std::string> cardList;
